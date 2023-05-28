87adda0587ad234cd6eb6b41d29454517fb8c9c9
juemuel <799525644@qq.com>
2023-05-28 20:39:00 +0800

    update 小程序路由

---
+++ uniapp_learning/uniapp_unicloud_demo/pages/user/index.vue
@@ -0,0 +1,81 @@
+<template>
+ <view class="my">
+  <view class="person_info">
+   <view class="avotor">
+    <image src="#" mode=""></image>
+   </view>
+   <view class="nackname_and_time">
+    <view class="nackname">一枚小吃货</view>
+    <view class="time">好好学习，天天向上</view>
+   </view>
+  </view>
+  <view class="line"></view>
+  <view class="info" @click="goToPersonInfo">
+   <text class="icon m-r-10">&#xe604;</text>
+   个人中心
+  </view>
+  <view class="line"></view>
+  <view class="info"><text class="icon m-r-10">&#xe6a9;</text>text1</view>
+  <view class="line"></view>
+  <view class="info"><text class="icon m-r-10">&#xe6ac;</text>text2</view>
+  <view class="line"></view>
+  <view class="info"><text class="icon m-r-10">&#xe62d;</text>text3</view>
+ </view>
+</template>
+
+<script>
+ export default {
+  data() {
+   return {
+
+   }
+  },
+  methods: {
+   goToPersonInfo(){
+    uni.navigateTo({
+     url: '#'
+    })
+   }
+  }
+ }
+</script>
+
+<style lang="scss" scoped>
+.my {
+ 
+}
+.m-r-10 {
+ margin-right: 10px;
+}
+.person_info {
+  display:flex;
+  align-items: center;
+  margin: 20px 0 0 20px;
+  .avotor {
+   >image {
+    height: 60px;
+    width: 60px;
+    border-radius: 50px;
+   }
+  }
+  .nackname_and_time {
+   margin-left: 10px;
+   .nackname {
+    
+   }
+   .time {
+    font-size: 14px;
+    color: gray;
+   }
+  }
+}
+.line {
+ height: 1px;
+ background: #CCCCCC;
+ margin: 10px 20px;
+}
+.info {
+ margin: 0px 0 0px 20px;
+ font-size: 16px;
+}
+</style>
--- uniapp_learning/uniapp_unicloud_demo/readme.md
+++ uniapp_learning/uniapp_unicloud_demo/readme.md
@@ -2,4 +2,4 @@
 ## 留言板小程序+unicloud
 ### 预览图
 
-<img src="https://s2.loli.net/2023/05/28/p8F65Z7GMNqV2eW.png" alt="image-20230528161809415" style="zoom:50%;" />
+<img src="https://s2.loli.net/2023/05/28/P6ZwnWHOJLs8Ch2.png" alt="image-20230528203753270" style="zoom:50%;" />
