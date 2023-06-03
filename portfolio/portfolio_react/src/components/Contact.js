4a94e4fe7f3b3d35f24764d6894cb93002122ad1
juemuel <799525644@qq.com>
2023-06-03 14:27:00 +0800

    finish portfoliov0.99 & remove error css

---
+++ portfolio/portfolio_react/src/components/Contact.js
@@ -0,0 +1,121 @@
+import React from "react";
+
+export default function Contact() {
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [message, setMessage] = React.useState("");
+
+  function encode(data) {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+      )
+      .join("&");
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", name, email, message }),
+    })
+      .then(() => alert("发送成功!"))
+      .catch((error) => alert(error));
+  }
+
+  return (
+    <section id="contact" className="relative">
+      <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+        <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+          <iframe
+            width="100%"
+            height="100%"
+            title="map"
+            className="absolute inset-0"
+            style={{ filter: "opacity(0.7)" }}
+            src="https://www.google.com/maps/embed/v1/place?q=中国浙江绍兴上虞区市民大道上虞市政府&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+            // src= "https://map.baidu.com/@13457093.01165841,3485564.532719562,14.2z"
+          />
+          <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
+            <div className="lg:w-1/2 px-6">
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                地址
+              </h2>
+              <p className="mt-1">
+                市民大道. <br />
+                上虞, 绍兴
+              </p>
+            </div>
+            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                邮箱
+              </h2>
+              <a className="leading-relaxed">
+                12345@qq.com
+              </a>
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
+                手机号
+              </h2>
+              <p className="leading-relaxed">123-456-7890</p>
+            </div>
+          </div>
+        </div>
+        <form
+          netlify="true"
+          name="contact"
+          onSubmit={handleSubmit}
+          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+          <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+            Contact Me
+          </h2>
+          <p className="leading-relaxed mb-5">
+            
+          </p>
+          <div className="relative mb-4">
+            <label htmlFor="name" className="leading-7 text-sm text-gray-400">
+              姓名
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="relative mb-4">
+            <label htmlFor="email" className="leading-7 text-sm text-gray-400">
+              邮箱
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="relative mb-4">
+            <label
+              htmlFor="message"
+              className="leading-7 text-sm text-gray-400">
+              内容
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+              onChange={(e) => setMessage(e.target.value)}
+            />
+          </div>
+          <button
+            type="submit"
+            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+            Submit
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+}
---
+++ portfolio/portfolio_react/src/data.js
@@ -0,0 +1,75 @@
+export const projects = [
+  {
+    title: "Share Stack",
+    subtitle: "Vitepress",
+    description:
+      "一个基于 vitepress 实现的技术分享Blog。包括月刊、随笔、面经、工作流等。",
+    image: "https://s2.loli.net/2023/06/03/rjFDTKpQ6tHs4Oh.png",
+    link: "https://juemuel.github.io/share_stack/",
+  },
+  {
+    title: "Kob Game",
+    subtitle: "Vue3、BootStrap && SpringBoot",
+    description:
+      "一个基于 Vue3、Bootstrap、Spring Boot 的多人联机游戏。",
+    image: "./logo192.png",
+    link: "#",
+  },
+  {
+    title: "Weather App",
+    subtitle: "Vue",
+    description:
+      "一个使用Vue2 和 API接口的地点实时天气查询App，可获取定位权限默认显示当地天气，也可按地点查询。",
+    image: "https://s2.loli.net/2023/06/03/msrLbfAi7kdtnl1.png",
+    link: "https://6476fc1bd18d7b7578c75d70--cozy-frangipane-de6138.netlify.app/",
+  },
+  {
+    title: "Web Common Tools",
+    subtitle: "JavaScript",
+    description:
+      "一个使用原生Javascript实现的Web通用工具，包括文件上传下载、颜色生成、二维码生成等等",
+    image: "https://s2.loli.net/2023/05/28/FBNJVKMEYzZPLec.png",
+    link: "#",
+  },
+  {
+    title: "Image && Video Gallery",
+    subtitle: "JavaScript",
+    description:
+      "一个使用原生Javascript实现的图片画廊，支持懒加载、响应式、图片下载、图片预览等",
+    image: "https://s2.loli.net/2023/05/19/Tnpzj39sWcHLAwb.png",
+    link: "#",
+  },
+  {
+    title: "More Proj",
+    subtitle: "Github Profile",
+    description:
+      "更多项目，请见Github",
+    image: "https://s2.loli.net/2023/06/03/2CPRIpik5eJmDBT.png",
+    link: "https://github.com/juemuel",
+  },
+];
+
+// export const testimonials = [
+//   {
+//     quote:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+//     image: "https://randomuser.me/api/portraits/men/1.jpg",
+//     name: "Jesse Hicks",
+//     company: "Zoozle",
+//   },
+//   {
+//     quote:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+//     image: "https://randomuser.me/api/portraits/men/94.jpg",
+//     name: "Ruben Alvarez",
+//     company: "Dooble.io",
+//   },
+// ];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Vue",
+  "Node",
+  "Linux",
+];
