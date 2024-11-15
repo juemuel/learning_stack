22e866b74531538fbe71e38cb61c3fee0c6e979e
Juemuel <799525644@qq.com>
2024-11-15 13:32:47 +0800

    update

--- web_frameworks_learning/flutter_learning/apptest/lib/web_view_page.dart
+++ web_frameworks_learning/flutter_learning/apptest/lib/web_view_page.dart
@@ -1,5 +1,38 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Permission.camera.request();
+  await Permission.microphone.request();
+
+  runApp(MyApp());
+}
+
+Future<void> requestPermissions() async {
+  Map<Permission, PermissionStatus> statuses = await [
+    Permission.camera,
+    Permission.microphone,
+  ].request();
+
+  if (statuses[Permission.camera] != PermissionStatus.granted ||
+      statuses[Permission.microphone] != PermissionStatus.granted) {
+    // 处理权限被拒绝的情况
+    print("需要摄像头和麦克风权限");
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: WebViewPage(),
+    );
+  }
+}
 
 class WebViewPage extends StatefulWidget {
   @override
@@ -17,21 +50,48 @@ class _WebViewPageState extends State<WebViewPage> {
       ),
       body: Container(
         child: InAppWebView(
-          initialUrlRequest: URLRequest(url: Uri.parse("https://www.baidu.com")),
+          initialUrlRequest: URLRequest(url: Uri.parse("https://webapptest.yhchmo.com/yh-video-doctor2024/#/")),
           onWebViewCreated: (controller) {
             webViewController = controller;
           },
           initialOptions: InAppWebViewGroupOptions(
             crossPlatform: InAppWebViewOptions(
               mediaPlaybackRequiresUserGesture: false,
+              javaScriptEnabled: true,
+              javaScriptCanOpenWindowsAutomatically: true,
+              allowFileAccessFromFileURLs: true,
+              allowUniversalAccessFromFileURLs: true,
             ),
             android: AndroidInAppWebViewOptions(
               useHybridComposition: true,
+              allowFileAccess: true,
+              allowContentAccess: true,
             ),
             ios: IOSInAppWebViewOptions(
               allowsInlineMediaPlayback: true,
+              allowsAirPlayForMediaPlayback: true,
+              allowsLinkPreview: true,
             ),
           ),
+          androidOnPermissionRequest: (InAppWebViewController controller, String origin, List<String> resources) async {
+            return PermissionRequestResponse(resources: resources, action: PermissionRequestResponseAction.GRANT);//主要是这行代码是授权的
+          },
+          shouldOverrideUrlLoading: (controller, navigationAction) async {
+            var uri = navigationAction.request.url!;
+            if (uri.scheme == 'baiduboxapp') {
+              return NavigationActionPolicy.CANCEL;
+            }
+            return NavigationActionPolicy.ALLOW;
+          },
+          onLoadStart: (controller, url) {
+            print("Loading started: $url");
+          },
+          onLoadStop: (controller, url) {
+            print("Loading finished: $url");
+          },
+          onConsoleMessage: (controller, consoleMessage) {
+            print(consoleMessage.message);
+          },
         ),
       ),
     );
